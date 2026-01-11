e93a1031afb845dcb8def8b370f7b41290b3f83e
hilmigas <haloomy2002@gmail.com>
2026-01-11 10:28:44 +0300

    final unit test homework

--- tests/test_component.py
+++ tests/test_component.py
@@ -2,7 +2,7 @@ from fastapi.testclient import TestClient
 from app.main import app
 
 client = TestClient(app)
-
+#
 def test_api_works():
     response = client.post("/predict", json={"value": "test"})
     assert response.status_code == 200
